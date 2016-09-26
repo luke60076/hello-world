cea7d7826d8b8213b302a8a8bdf5368c284f1cdc
luke60076 <luke60076@gmail.com>
2016-09-26 22:52:04 +0200

    Rebase 001

--- HelloWorld/HelloWorldConsole/Program.cs
+++ HelloWorld/HelloWorldConsole/Program.cs
@@ -12,7 +12,7 @@ namespace HelloWorldConsole
         {
             Console.WriteLine("Hello New World 2nd Edition!!!");
             Console.WriteLine("My second application.");
-	        Console.WriteLine("Press any key to continue ...");
+	        Console.WriteLine("Press any key to exit ...");
 	        Console.ReadKey();
         }
     }
